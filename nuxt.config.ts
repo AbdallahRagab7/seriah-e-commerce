8be8e6b42ba5c837e7c6637e09bfcfcb154dc170
Abdallah Ragab <abdallahragab.runprof@gmail.com>
2024-09-28 21:42:09 +0300

    project structure

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/typescript-build"],
+});
+
+// nuxt config
+// import type { NuxtConfig } from "@nuxt/types";
+
+// const config: NuxtConfig = {
+//   buildModules: ["@nuxt/typescript-build"],
+//   devtools: { enabled: true },
+// };
+
+// export default config;
