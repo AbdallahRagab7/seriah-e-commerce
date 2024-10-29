ea42877b3951936fa68f0ba841fe8c954cd0c2a5
Abdallah Ragab <abdallahragab.runprof@gmail.com>
2024-10-29 14:41:59 +0300

    remove cart line

--- pages/cart.vue
+++ pages/cart.vue
@@ -3,7 +3,7 @@
     <div class="grid md:grid-cols-3 gap-14 md:gap-4 lg:gap-8">
       <!--START ITEMS CONTAINER -->
       <div class="md:col-span-2 p-4 lg:px-8 bg-gray-50 rounded-md">
-        <h2 class="heading1 !text-[29px]">Cart</h2>
+        <h2 class="!text-[27px]">Cart</h2>
         <!-- {{ items }} -->
         <hr class="border-gray-300 mt-4 mb-8" />
         <h1
