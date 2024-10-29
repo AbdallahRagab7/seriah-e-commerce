0115c45f8388c3fc4b2529e551caec743268087b
Abdallah Ragab <abdallahragab.runprof@gmail.com>
2024-10-29 13:48:30 +0300

    add price and aspect ratio for adCard

--- components/product/adCard.vue
+++ components/product/adCard.vue
@@ -5,7 +5,7 @@
         <NuxtImg
           :src="`${$config.public.STRAPI_URL}${product?.attributes?.main_image?.data?.attributes?.url}`"
           alt="product image"
-          class="w-full h-full object-cover transform transition-transform duration-300 group-hover:scale-105"
+          class="w-full aspect-[310/149] object-cover transform transition-transform duration-300 group-hover:scale-105"
         />
       </div>
     </NuxtLink>
@@ -25,7 +25,8 @@
           class="text-[#323232] hover:text-primary flex items-center justify-center text-[13px] font-semibold mt-5 bg-transparent border-2 py-3 px-8 border-[#323232]"
         >
           Buy Now /
-          <span class="mx-1 text-[10px] font-medium">EGP</span> 100
+          <span class="mx-1 text-[10px] font-medium">EGP</span>
+          {{ product?.attributes?.price }}
         </button>
       </NuxtLink>
     </div>
