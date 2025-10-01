288a94b0cbc2ea6ee3ef21e6b76b7f416f7aa6ab
Abdallah Ragab <91810306+AbdallahRagab7@users.noreply.github.com>
2025-10-01 12:46:59 +0300

    Update error.vue

--- error.vue
+++ error.vue
@@ -16,7 +16,7 @@
 <script setup lang="ts">
 // import type { NuxtError } from '#app'
 
-// const props = defineProps<{ error: NuxtError }>()
+// const propss = defineProps<{ error: NuxtError }>()
 </script>
 
 <style scoped></style>
